2707c2d28d4410bc0bc8565cfad1a63313a89675
cafe51 <cafecafe51@hotmail.com>
2023-07-01 07:44:00 -0400

    feat: endpoint inicial de boas vindas

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,17 @@
+/* eslint-disable max-len */
 import express from 'express';
 import vehicleRouter from './Routes/vehicleRouter';
 import ErrorHandler from './Middlewares/ErrorHandler';
 
 const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    instruções: 'A api está pronta para uso! Siga a documentação da api para conhecer os endpoints',
+    documentação: 'https://github.com/cafe51/car-shop',
+  });
+});
+
 app.use(express.json());
 app.use(vehicleRouter);
 app.use(ErrorHandler.handle);
