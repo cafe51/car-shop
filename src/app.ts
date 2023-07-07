13bc0aa8557fdfeadddf8c1580840977e2453fdd
cafe51 <cafecafe51@hotmail.com>
2023-07-07 18:08:37 -0400

    adicionado cabeçalho cors para as requisições

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,12 @@ import ErrorHandler from './Middlewares/ErrorHandler';
 
 const app = express();
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.get('/', (req, res) => {
   res.status(200).json({
     instruções: 'A api está pronta para uso! Siga a documentação da api para conhecer os endpoints',
